56a6f05e57444cb5b652506deb84dd5b689e0ae8
yuny0623 <overman522@gmail.com>
2023-02-04 14:34:33 +0900

    refactor: Refactor to divide ImageGhost client side by run two threads for get user input and to receive server response

--- src/main/java/org/pgp/openchat/DisplayThread.java
+++ src/main/java/org/pgp/openchat/DisplayThread.java
@@ -1,4 +1,83 @@
 package org.pgp.openchat;
 
-public class DisplayThread {
+import org.pgp.utils.json.JsonUtil;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DisplayThread extends Thread {
+
+    Socket socket;
+    String strIn;
+    BufferedReader in;
+    Map<String, String> parsedJson;
+
+    public DisplayThread(Socket socket){
+        this.socket = socket;
+        parsedJson = new HashMap<>();
+    }
+
+    public boolean isValidInput(String input){
+        if(input == null || input.isEmpty() || input.isBlank()){
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isValidParsedJson(String parsedJson){
+        if(parsedJson == null || parsedJson.isEmpty() || parsedJson.isBlank()){
+            System.out.println("[Error] parsing json error.");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void run(){
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(true){
+            try {
+                strIn = in.readLine();
+
+                // input validation check
+                if(!isValidInput(strIn)){
+                    continue;
+                }
+
+                parsedJson = JsonUtil.parseJson(strIn);
+
+                if(parsedJson.get("publicKey").isEmpty()){
+                    System.out.println("[Client] Invalid Input.");
+                    continue;
+                }
+
+                System.out.println("[Notice] " + parsedJson.get("publicKey") + ":" + parsedJson.get("message"));
+            }catch(Exception e){
+                if(e.getMessage().equals("Connection reset")) {
+                    System.out.println("[Error] Socket - " + e.getMessage());
+                    System.out.println("Server is not running...");
+                }else{
+                    System.out.println(e.getMessage());
+                }
+                System.out.println("Exit Client.");
+                try {
+                    socket.close();
+                    in.close();
+                }catch(IOException err){
+                    err.printStackTrace();
+                    break;
+                }
+                break;
+            }
+        }
+    }
 }
--- src/main/java/org/pgp/openchat/InputThread.java
+++ src/main/java/org/pgp/openchat/InputThread.java
@@ -1,4 +1,56 @@
 package org.pgp.openchat;
 
-public class InputThread {
+import org.pgp.utils.json.JsonUtil;
+import org.pgp.wallet.KeyWallet;
+
+import java.io.*;
+import java.net.Socket;
+
+public class InputThread extends Thread{
+    Socket socket;
+    PrintWriter out;
+    BufferedReader br;
+    String clientInput;
+    String json;
+    String nickname;
+
+
+    public InputThread(Socket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run(){
+        try {
+            out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            br = new BufferedReader(new InputStreamReader(System.in));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // 게임 진행
+        while(true){
+            try {
+                clientInput = br.readLine();
+
+                json = JsonUtil.generateJson(clientInput, KeyWallet.getMainASymmetricKey().getPublicKey());
+                if(json.isEmpty() || json.isBlank()){
+                    System.out.println("Invalid Command.");
+                    continue;
+                }
+                out.println(json);
+            } catch (IOException e) {
+                e.printStackTrace();
+                try {
+                    socket.close();
+                    out.close();
+                    br.close();
+                }catch(IOException err){
+                    err.printStackTrace();
+                    System.exit(1);
+                }
+                break;
+            }
+        }
+    }
 }
